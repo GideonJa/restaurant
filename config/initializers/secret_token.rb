b90716bb6d115d746de7d2dd094a10a99d9410b0
Gideon jadlovker <gidkj2000@yahoo.com>
2011-08-09 08:48:38 -0400

    initial commit2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NyuRest::Application.config.secret_token = 'eb541bb885682ec69dd9d4ff888da3da702e8cb5fecff8f0029fad224d583fb762619b17c0d47a3033696b890490a19115a153621fa6f1b830621a2cccf0ca4e'
---
+++ readme.markdown
@@ -0,0 +1,5 @@
+# Ruby on Rails class at NYU
+
+This is the final project for the Ruby and Rails class at NYU SCPS Aug 2011.
+
+by [Gideon J](http://www.linkedin.com/in/gideonj ).
