440adf1a52b85a5f299aa1596cbdbe5c44716162
Gideon jadlovker <gidkj2000@yahoo.com>
2011-08-09 11:28:25 -0400

    added restaurant CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 NyuRest::Application.routes.draw do
+ 
+
+  root :to => 'restaurants#index'
+  get "restaurants/list"
+  get "restaurants/new"
+  get "restaurants/delete"
+  post "restaurants/create"
+ 
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,5 +63,5 @@ NyuRest::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+   match ':controller(/:action(/:id(.:format)))'
 end
